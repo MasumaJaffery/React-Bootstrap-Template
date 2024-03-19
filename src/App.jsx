eff77b5bbe01f4b3e259d5a069af98065d36cebc
MasumaJaffery <masumajaffery1@gmail.com>
2024-03-19 21:08:12 +0500

    Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css'
+import Intro from './components/intro';
+function App() {
+  return (
+    <>
+     <Intro />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/intro.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Intro = () => {
+ return (
+  <>
+  <button type="button" className="btn btn-primary">Salam React</button>
+  </>
+ );
+}
+export default Intro;
